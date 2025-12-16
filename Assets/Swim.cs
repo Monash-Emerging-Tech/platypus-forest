39b6ee21b129c1fe356b0287c81c1b17626bc079
Clarisse Tandra <ctan0170@student.monash.edu>
2025-12-17 01:38:54 +0700

    Added the platypus swimming in circle + waddle animation (animation too small)

---
+++ Assets/Swim.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+     public Transform centerPoint;   
+    public float radius = 2f;
+    public float speed = 1f;
+     private float angle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle += speed * Time.deltaTime;
+
+        float x = Mathf.Cos(angle) * radius;
+        float z = Mathf.Sin(angle) * radius;
+
+        transform.position = centerPoint.position + new Vector3(x, 0, z);
+
+     
+        Vector3 direction = new Vector3(-Mathf.Sin(angle), 0, Mathf.Cos(angle));
+        transform.rotation = Quaternion.LookRotation(direction);
+    }
+}
