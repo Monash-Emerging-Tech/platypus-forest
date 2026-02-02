c6310ee917391d676d4d78fb9294d981973e121e
pandgey <krittapong.ti@gmail.com>
2026-02-02 13:41:35 +0700

    implemented mesh collider on bridges in Island2 scene, fixed the Main Camera, and added comment on SceneSwitcher.cs

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -9,7 +9,9 @@ public class SceneSwitcher : MonoBehaviour
     // Start is called before the first frame update
     public void EndGame(bool success)
     {
-        SceneManager.LoadScene(0);
+
+        // Change the number of the .LoadScene(0) accordingly on the Scenes in Build settings
+        SceneManager.LoadScene(1);
     }
 
 }
